b8cf12aa4707ca6863a805b50a2cadfe5dfea9d1
Christine Nguyen <christine.nguyen@airbnb.com>
2017-03-28 23:03:29 -0700

    basic nytimes search app from video walkthrough

---
+++ app/src/main/java/com/codepath/nytimessearch/models/Article.java
@@ -0,0 +1,62 @@
+package com.codepath.nytimessearch.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ * Created by christine_nguyen on 3/28/17.
+ */
+
+public class Article implements Serializable {
+    String webUrl;
+    String headline;
+    String thumbnail;
+
+    public String getWebUrl() {
+        return webUrl;
+    }
+
+    public String getHeadline() {
+        return headline;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public Article(JSONObject jsonObject) {
+        try {
+            this.webUrl = jsonObject.getString("web_url");
+            this.headline = jsonObject.getJSONObject("headline").getString("main");
+
+            JSONArray multimedia = jsonObject.getJSONArray("multimedia");
+
+            if (multimedia.length() >= 1) {
+                JSONObject multimediaJson = multimedia.getJSONObject(0);
+                this.thumbnail = "http://www.nytimes.com/" + multimediaJson.getString("url");
+            } else {
+                this.thumbnail = "";
+            }
+        } catch (JSONException e) {
+
+        }
+    }
+
+    public static ArrayList<Article> fromJsonArray(JSONArray array) {
+        ArrayList<Article> results = new ArrayList<>();
+
+        for (int i = 0; i < array.length(); i++) {
+            try {
+                results.add(new Article(array.getJSONObject(i)));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return results;
+    }
+}
